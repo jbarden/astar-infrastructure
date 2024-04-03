d5adb138b4f12d50322be5ae87269c052366135f
Jason Barden <jason.barden@outlook.com>
2024-04-03 23:07:45 +0100

    Add code from previous version which had Data.Data in namespace...

---
+++ AStar.Infrastructure/Data/FilesContext.cs
@@ -0,0 +1,15 @@
+﻿using AStar.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AStar.Infrastructure.Data;
+
+public class FilesContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("Filename=c:\\temp\\Files.db");
+
+    public DbSet<FileDetail> Files { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+        => _ = modelBuilder.Entity<FileDetail>().HasKey(vf => new { vf.FileName, vf.DirectoryName });
+}
